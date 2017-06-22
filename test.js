945cbfe2ede8b4419141127bbd131ba8ed6f90d2
Brandon Him <brh55@drexel.edu>
2017-06-22 14:03:10 -0500

    Add COUNT test

--- test.js
+++ test.js
@@ -5,13 +5,22 @@ test('Convert FREQ', t => {
 	const PREFIX = "RRULE:FREQ=";
 	// iCal: "HOURLY" / "DAILY" / "WEEKLY" / "MONTHLY" / "YEARLY"
 	// WX: DAILY, WEEKLY, NO_REPEAT, MONTHLY, YEARLY and CONSTANT
-	const DAILY = `${PREFIX}DAILY`;
-	const WEEKLY = `${PREFIX}WEEKLY`;
-	const MONTHLY = `${PREFIX}MONTHLY`;
-	const YEARLY = `${PREFIX}YEARLY`;
+	const DAILY = 'DAILY';
+	const WEEKLY = 'WEEKLY';
+	const MONTHLY = 'MONTHLY';
+	const YEARLY = 'YEARLY';
 
 	t.is(convert.freq(WEEKLY), '<repeatType>WEEKLY</repeatType>');
 	t.is(convert.freq(DAILY), '<repeatType>DAILY/repeatType>');
 	t.is(convert.freq(MONTHLY), '<repeatType>MONTHLY</repeatType>');
 	t.is(convert.freq(YEARLY), '<repeatType>YEARLY</repeatType>');
+});
+
+test('Convert COUNT', t => {
+	// iCAL: COUNT -
+	// WX: interval - [1 ... 99]
+	t.is(convert.count(1), '<afterMeetingNumber>1</afterMeetingNumber>');
+	t.is(convert.count(99), '<afterMeetingNumber>99</afterMeetingNumber>');
+	t.throws(() => convert.count(99), 'WebEx does not support numbers greater than 99');
+	t.throws(() => convert.count(0), 'Expects a number greater than 0, received 0');
 });
