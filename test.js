55c87004499066f5c0a5704370fc298f2d76e015
Brandon Him <brh55@drexel.edu>
2017-06-22 14:28:47 -0500

    Add BYMONTH test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import RRule from 'rrule';
 import convert from '.';
 
-test('Convert FREQ', t => {
+test('Convert FREQ', () => {
 	const PREFIX = "RRULE:FREQ=";
 	// iCal: "HOURLY" / "DAILY" / "WEEKLY" / "MONTHLY" / "YEARLY"
 	// WX: DAILY, WEEKLY, NO_REPEAT, MONTHLY, YEARLY and CONSTANT
@@ -17,7 +17,7 @@ test('Convert FREQ', t => {
 	t.is(convert.freq(YEARLY), '<repeatType>YEARLY</repeatType>');
 });
 
-test('Convert COUNT', t => {
+test('Convert COUNT', () => {
 	// iCAL: COUNT -
 	// WX: afterMeetingNumb - [1 ... 999]
 	t.is(convert.count(1), '<afterMeetingNumber>1</afterMeetingNumber>');
@@ -26,7 +26,7 @@ test('Convert COUNT', t => {
 	t.throws(() => convert.count(-1), 'Expects a number greater than 0, received -1');
 });
 
-test('Convert INTERVAL', t => {
+test('Convert INTERVAL', () => {
 	// iCAL: COUNT -
 	// WX: INTERVAL - [1 ... 99]
 
@@ -36,7 +36,7 @@ test('Convert INTERVAL', t => {
 	t.throws(() => convert.interval(0), 'Expects a number greater than 1, received -1');
 });
 
-test('Convert BYDAY/byweekday', t => {
+test('Convert BYDAY/byweekday', () => {
 	t.is(convert.byweekday([RRule.MO, RRule.SU]),
 		 '<dayInWeek><day>MONDAY</day><day>SUNDAY</day></dayInWeek>');
 	t.is(convert.byweekday(RRule.TU),
@@ -51,10 +51,29 @@ test('Convert BYDAY/byweekday', t => {
 		 '<dayInWeek><day>MONDAY</day><day>SATURDAY</day></dayInWeek>');
 });
 
-test('Convert BYMONTHDAY', t => {
+test('Convert BYMONTHDAY', () => {
 	t.is(convert.bymonthday(31), '<dayInMonth>31</dayInMonth>');
 	t.is(convert.bymonthday(1), '<dayInMonth>1</dayInMonth>');
 
 	t.throws(() => convert.bymonthday(0), 'Expects a number greater than 0, received 0');
 	t.throws(() => convert.bymonthday(32), 'Expects a number less than 32, received 32');
+});
+
+test('Convert UNTIL', () => {
+
+});
+
+test('Convert BYWEEKNO', () => {
+
+});
+
+test('convert BYMONTH', () => {
+	// iCal: BYMONTH
+	// W: <monthInYear>
+
+	t.is(convert.bymonth(1), '<monthInYear>1</monthInYear>');
+	t.is(convert.bymonth(12), '<monthInYear>12</monthInYear>');
+
+	t.throws(() => convert.bymonth(13), 'Expects a number less than 13, received 13');
+	t.throws(() => convert.bymonth(0), 'Expects a number greater than 0, received 0');
 });
