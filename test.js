384a6e87672974440a08cca33f7eaed36e97e024
Brandon Him <brh55@drexel.edu>
2017-06-22 13:52:37 -0500

    Add frequency test

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 import test from 'ava';
-import fn from '.';
+import convert from '.';
 
-test('title', t => {
-	const err = t.throws(() => fn(123), TypeError);
-	t.is(err.message, 'Expected a string, got number');
+test('Convert FREQ', t => {
+	const PREFIX = "RRULE:FREQ=";
+	// iCal: "HOURLY" / "DAILY" / "WEEKLY" / "MONTHLY" / "YEARLY"
+	// WX: DAILY, WEEKLY, NO_REPEAT, MONTHLY, YEARLY and CONSTANT
+	const DAILY = `${PREFIX}DAILY`;
+	const WEEKLY = `${PREFIX}WEEKLY`;
+	const MONTHLY = `${PREFIX}MONTHLY`;
+	const YEARLY = `${PREFIX}YEARLY`;
 
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+	t.is(convert.freq(WEEKLY), '<repeatType>WEEKLY</repeatType>');
+	t.is(convert.freq(DAILY), '<repeatType>DAILY/repeatType>');
+	t.is(convert.freq(MONTHLY), '<repeatType>MONTHLY</repeatType>');
+	t.is(convert.freq(YEARLY), '<repeatType>YEARLY</repeatType>');
 });
